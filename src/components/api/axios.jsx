697e328606388d6c790f3a078817549522543aec
Naomi Pham <phambaonguyendn@gmail.com>
2023-03-29 23:50:46 +0700

    Rebuild file

--- src/components/api/axios.jsx
+++ src/components/api/axios.jsx
@@ -1,20 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log('🚀 ~ file: axios.jsx:4 ~ BASE_URL:', BASE_URL);
 
 export default axios.create({
-  baseURL: BASE_URL,
+	baseURL: BASE_URL,
 });
 
 axios.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    if (error.response.status === 404) {
-      console.log(404);
-    }
-    console.error(res.status);
-    return Promise.reject(error);
-  }
+	function (response) {
+		return response;
+	},
+	function (error) {
+		if (error.response.status === 404) {
+			console.log(404);
+		}
+		console.log(error);
+		return Promise.reject(error);
+	},
 );
