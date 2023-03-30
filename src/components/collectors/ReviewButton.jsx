7e97005058f11d956d6671c64569569afa28cb3b
Naomi Pham <phambaonguyendn@gmail.com>
2023-03-30 18:22:26 +0700

    Update review button

--- src/components/collectors/ReviewButton.jsx
+++ src/components/collectors/ReviewButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReviewButton = () => {
 	return (
-		<div className="cozy-rounded-md cozy-border-[2px] cozy-border-brand cozy-p-3 cozy-text-center hover:cozy-cursor-pointer hover:cozy-bg-rose-200 hover:cozy-shadow-sm">
+		<div className="cozy-rounded-md cozy-border-2 cozy-border-brand cozy-p-3 cozy-text-center hover:cozy-cursor-pointer hover:cozy-bg-rose-200 hover:cozy-shadow-sm">
 			<a href="/" className="cozy-text-dark cozy-duration-150">
 				<span>Review us on</span>{' '}
 				<span className="cozy-font-primary cozy-font-extrabold cozy-text-brand">
