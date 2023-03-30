fb6f7bcdf13f0a0aee72d85dc3036277b990160b
Naomi Pham <phambaonguyendn@gmail.com>
2023-03-30 16:09:40 +0700

    Change tailwind classname prefix

--- src/components/essentials/Horizontal.jsx
+++ src/components/essentials/Horizontal.jsx
@@ -5,6 +5,7 @@ const Horizontal = () => {
 	return (
 		<div className="cozy-flex cozy-w-full cozy-flex-wrap cozy-items-center cozy-p-2 cozy-py-4">
 			<CozyStatsSmall withScore />
+
 			<a href="/" className="cozy-font-primary cozy-text-brand">
 				Cozy Cot
 			</a>
