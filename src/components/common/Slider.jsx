353e4bc1a8fef9f33600c341b4126642b2d46f66
Naomi Pham <phambaonguyendn@gmail.com>
2023-04-04 13:45:13 +0700

    Fix bug slider

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -1,63 +1,81 @@
 /* eslint-disable react/prop-types */
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ArrowLeft, ArrowRight } from '../icon/Arrow';
-import ReviewCard from './ReviewCard';
+import { throttle } from '../../helpers/Helpers.jsx';
+import ReviewCardMultiple from './ReviewCardMultiple';
 
 const Slider = ({ slides }) => {
 	const ref = useRef(null);
+	const totalSlide = slides?.length;
+	console.log('🚀 ~ file: Slider.jsx:10 ~ Slider ~ totalSlide:', totalSlide);
 	const [slidePosition, setSlidePosition] = useState(0);
-	const totalSlides = slides.length;
+	console.log(
+		'🚀 ~ file: Slider.jsx:11 ~ Slider ~ slidePosition:',
+		slidePosition,
+	);
+	const [translateX, setTranslateX] = useState(0);
+	const postsPerSlide = 2;
 
-	const handleClick = (direction) => () => {
-		if (ref.current) {
-			const { scrollLeft, clientWidth } = ref.current;
+	useEffect(() => {
+		setTranslateX(ref.current.clientWidth * slidePosition);
+	}, []);
 
-			if (direction === 'left') {
-				setSlidePosition((prevSlidePosition) => prevSlidePosition - 1);
-			} else {
-				setSlidePosition((prevSlidePosition) => prevSlidePosition + 1);
-			}
-
-			const scrollTo =
-				direction === 'left'
-					? scrollLeft - clientWidth
-					: scrollLeft + clientWidth;
-
-			ref.current.scrollTo({ left: scrollTo, behavior: 'smooth' });
+	async function moveToRight() {
+		if (slidePosition === totalSlide - 1) {
+			setSlidePosition(0);
+			setTranslateX(0);
+		} else {
+			setSlidePosition((prevSlidePosition) => prevSlidePosition + 1);
+			setTranslateX(ref.current.clientWidth * (slidePosition + 1));
 		}
-	};
+	}
+
+	async function moveToLeft() {
+		if (slidePosition === 0) {
+			setSlidePosition(totalSlide - 1);
+			setTranslateX(ref.current.clientWidth * (totalSlide - 1));
+		} else {
+			setSlidePosition((prevSlidePosition) => prevSlidePosition - 1);
+			setTranslateX(ref.current.clientWidth * (slidePosition - 1));
+		}
+	}
 
 	return (
-		<div className="cozy-relative cozy-mx-auto cozy-flex cozy-w-full cozy-max-w-3xl cozy-items-center cozy-gap-6">
+		<div className="cozy-relative cozy-mx-auto cozy-w-full">
+			<div className="cozy-mx-auto cozy-w-10/12 cozy-overflow-hidden">
+				<div
+					className="cozy-mx-auto cozy-flex cozy-items-center cozy-transition-transform cozy-duration-500 cozy-ease-in-out"
+					ref={ref}
+					style={{
+						transform: `translate3d(-${translateX}px, 0px, 0px)`,
+					}}
+				>
+					{slides?.map((item) => (
+						<ReviewCardMultiple key={item.id} item={item} />
+					))}
+				</div>
+			</div>
+
 			<button
-				className="cozy-h-full cozy-w-8 -cozy-translate-y-1/2 cozy-cursor-pointer cozy-transition-all cozy-duration-200 hover:cozy-scale-125 disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40"
-				onClick={handleClick('left')}
+				className="cozy-absolute cozy-left-0 cozy-bottom-0 cozy-top-0 cozy-h-full disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40"
+				onClick={throttle(() => moveToLeft(), 200)}
 				disabled={slidePosition === 0}
 			>
 				<ArrowLeft className="cozy-h-5 cozy-w-8 cozy-fill-light-neutral-700" />
 			</button>
 
-			<div
-				ref={ref}
-				className="cozy-flex cozy-items-start cozy-overflow-x-hidden"
-			>
-				{slides?.map((item) => (
-					<ReviewCard key={item.id} item={item} responsive />
-				))}
-			</div>
-
 			<button
-				className="cozy-hidden cozy-h-full cozy-w-8 -cozy-translate-y-1/2 cozy-cursor-pointer cozy-transition-all cozy-duration-200 hover:cozy-scale-125 disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40 sm:cozy-block"
-				onClick={handleClick('right')}
-				disabled={slidePosition === totalSlides / 2 - 1}
+				className="cozy-absolute cozy-right-0 cozy-bottom-0 cozy-top-0 cozy-hidden cozy-h-full disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40 sm:cozy-block"
+				onClick={throttle(() => moveToRight(), 200)}
+				disabled={slidePosition === Math.ceil(totalSlide / postsPerSlide) - 1}
 			>
 				<ArrowRight className="cozy-h-5 cozy-w-8 cozy-fill-light-neutral-700" />
 			</button>
 
 			<button
-				className="sm:hidden cozy-h-full cozy-w-8 -cozy-translate-y-1/2 cozy-cursor-pointer cozy-transition-all cozy-duration-200 hover:cozy-scale-125 disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40"
-				onClick={handleClick('right')}
-				disabled={slidePosition === totalSlides - 1}
+				className="cozy-absolute cozy-right-0 cozy-bottom-0 cozy-top-0 cozy-h-full disabled:cozy-cursor-not-allowed disabled:cozy-opacity-40 sm:cozy-hidden"
+				onClick={throttle(() => moveToRight(), 200)}
+				disabled={slidePosition === totalSlide - 1}
 			>
 				<ArrowRight className="cozy-h-5 cozy-w-8 cozy-fill-light-neutral-700" />
 			</button>
