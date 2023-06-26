2f721e4fb3c56665fc504010c13132d13f4dd1d6
naomipham <phambaonguyendn@gmail.com>
2023-06-26 17:54:17 +0700

    update logo

--- src/components/essentials/MicroReviewCount.jsx
+++ src/components/essentials/MicroReviewCount.jsx
@@ -5,7 +5,7 @@ import { IconLogo } from '../common/Icons';
 const MicroReviewCount = ({ website }) => {
 	return (
 		<p
-			className="cozy-flex cozy-justify-center cozy-gap-2 cozy-font-medium cozy-text-light-neutral-800 cozy-outline"
+			className="cozy-flex cozy-justify-center cozy-gap-2 cozy-font-medium cozy-text-light-neutral-800"
 			style={{ alignItems: 'end' }}
 		>
 			See our reviews on
